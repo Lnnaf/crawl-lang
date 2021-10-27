59c4b8ccba3963e924f7b73b431057d3e0112a45
Linh Nguyen <vanlinh12b5@gmail.com>
2021-10-27 20:53:30 +0700

    init crawl languages from google api page

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.exec.util.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.json.Json;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+   public static WebDriver driver;
+    public static final String PATH_DRIVER = "C:\\Users\\vanli\\Downloads\\chromedriver_win32\\chromedriver.exe";
+    public static final String XPATH_NAME_LANG = "/html/body/section/section/main/devsite-content/article/div[2]/div/table/tbody/tr/td";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",PATH_DRIVER);
+        driver = new ChromeDriver();
+        driver.get("https://developers.google.com/admin-sdk/directory/v1/languages");
+        List<WebElement>  names = driver.findElements(By.xpath(XPATH_NAME_LANG));
+        Lang lang = new Lang();
+        List<Lang> langs = new ArrayList<>();
+        for(WebElement name: names){
+            if(lang.getName()==null) {
+                lang.setName(name.getText());
+                continue;
+            }else{
+                lang.setCode(name.getText());
+            }
+            langs.add(lang);
+            lang = new Lang();
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(langs);
+        System.out.println(json);
+        try {
+            FileWriter writer = new FileWriter("MyFile.txt", true);
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        driver.close();
+    }
+}
